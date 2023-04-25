95d09fe25bdb98b04e5a73146864623dbeb39614
Yang, Jenny <jyang741@gatech.edu>
2023-04-24 23:11:31 -0400

    Update Decoder class
    - initialized each subcomponent with nn.Sequential for readability
    - implemented dropout layers
    - added skip connections
    - added final normalization and dropout for output layer
    - added initial dropout for inputs

--- tasks/Decoder.py
+++ tasks/Decoder.py
@@ -1,3 +1,5 @@
+# Ref: https://github.com/huggingface/transformers/blob/main/src/transformers/models/t5/modeling_t5.py
+
 import torch
 import torch.nn as nn
 
@@ -6,7 +8,7 @@ class Decoder(nn.Module):
         You will need to complete the init function and the forward function.
     """
 
-    def __init__(self, hidden_size, num_heads, dropout=0.2):
+    def __init__(self, hidden_size, num_heads, dropout=0.1):
         super(Decoder, self).__init__()
 
         # initialize model parameters
@@ -15,44 +17,51 @@ class Decoder(nn.Module):
         self.dropout = dropout
 
         # initialize model layers
-        self.masked_self_attention = torch.nn.MultiheadAttention(self.hidden_size, self.num_heads) #TODO add dropout parameter here?
-        self.norm1 = nn.LayerNorm(self.hidden_size)
-        self.encoder_decoder_attention = torch.nn.MultiheadAttention(self.hidden_size, self.num_heads)
-        self.norm2 = nn.LayerNorm(self.hidden_size)
-        self.feedforward1 = nn.Linear(self.hidden_size, self.hidden_size)
-        self.relu = nn.ReLU()
-        self.feedforward2 = nn.Linear(self.hidden_size, self.hidden_size)
-        self.norm3 = nn.LayerNorm(self.hidden_size)
+        self.dropout_input = nn.Dropout(p=dropout)
+        self.masked_self_attention = nn.Sequential(nn.LayerNorm(self.hidden_size),
+                                                   torch.nn.MultiheadAttention(self.hidden_size, self.num_heads, dropout=dropout),
+                                                   nn.Dropout(p=dropout)
+                                                   )
+        
+        self.encoder_decoder_attention = nn.Sequential(nn.LayerNorm(self.hidden_size),
+                                                       torch.nn.MultiheadAttention(self.hidden_size, self.num_heads, dropout=dropout),
+                                                       nn.Dropout(p=dropout)
+                                                       )
 
-    # TODO Add drop out layers
-    # - page 4 of T5 paper: Dropout (Srivastava et al., 2014) is applied within the feed-forward network, on the skip connection, on the attention weights, and at the input and output of the entire stack.
-    # - Vaswani, 2017: We apply dropout [33] to the output of each sub-layer, before it is added to thesub-layer input and normalized. In addition, we apply dropout to the sums of the embeddings and the
-    # positional encodings in both the encoder and decoder stacks. For the base model, we use a rate of
-    # Pdrop = 0.1.
+        # feedforward: Two linear transformations with a ReLU activation in between (Vaswani, 2017)
+        self.feedforward = nn.Sequential(nn.LayerNorm(self.hidden_size),
+                                         nn.Linear(self.hidden_size, self.hidden_size),
+                                         nn.ReLU(),
+                                         nn.Dropout(p=dropout),
+                                         nn.Linear(self.hidden_size, self.hidden_size)
+                                         )
+        self.norm_output = nn.LayerNorm(self.hidden_size)
+        self.dropout_output = nn.Dropout(p=dropout)
 
     def forward(self, inputs, enc_output, attn_mask):
+        # dropout at the input of the entire stack
+        hidden_states = self.dropout_input(inputs)
+
         # multihead self attention
-        masked_self_attention = self.masked_self_attention(inputs, inputs, inputs, attn_mask= attn_mask)
+        masked_self_attention, _ = self.masked_self_attention(hidden_states, hidden_states, hidden_states, attn_mask=attn_mask)
 
-        # add and normalize #TODO check implementation per page 4 of T5 paper: After layer normalization, a residual skip connection (He et al., 2016) adds each subcomponent’s input to its output.
-        add_residual1 = inputs + masked_self_attention[0]
-        norm1 = self.norm1(add_residual1)
-
+        # residual skip connection adds each subcomponent’s input to its output
+        skip_connection1 = hidden_states + masked_self_attention
+        
         # encoder-decoder multihead attention
         # In "encoder-decoder attention" layers, the queries come from the previous decoder layer, and the memory keys and values come from the output of the encoder. (Vaswani, 2017)
-        encoder_decoder_attention = self.encoder_decoder_attention(norm1, enc_output, enc_output)
+        encoder_decoder_attention, _ = self.encoder_decoder_attention(skip_connection1, enc_output, enc_output)
 
-        # add and normalize
-        add_residual2 = norm1 + encoder_decoder_attention[0]
-        norm2 = self.norm2(add_residual2)
+        # residual skip connection adds each subcomponent’s input to its output
+        skip_connection2 = skip_connection1 + encoder_decoder_attention
+        
+        # feed forward network
+        feedforward = self.feedforward(skip_connection2)
 
-        # feed forward: Two linear transformations with a ReLU activation in between (Vaswani, 2017)
-        feedforward1 = self.feedforward1(norm2)
-        relu = self.relu(feedforward1)
-        feedforward2 = self.feedforward2(relu)
+        # residual skip connection adds each subcomponent’s input to its output
+        skip_connection3 = skip_connection2 + feedforward
 
-        add_residual3 = norm2 + feedforward2
-        norm3 = self.norm3(add_residual3)
+        # normalization and dropout at the output of the entire stack
+        output = self.dropout_output(self.norm_output(skip_connection3))
 
-        output = norm3
         return output
