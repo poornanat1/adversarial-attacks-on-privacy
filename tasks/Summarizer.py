96047e154a9f0cb1251021b15d0397596ec6b4f1
Yang, Jenny <jyang741@gatech.edu>
2023-05-02 17:33:07 -0400

    Revert "Revert back to range(self.max_length) in Summarizer"
    This reverts commit f4353f3c0a823ddbc95125beb7c1e66983b63a7e.

--- tasks/Summarizer.py
+++ tasks/Summarizer.py
@@ -80,7 +80,8 @@ class Summarizer(nn.Module):
 
         # ended_sequences = set()
         
-        for i in range(self.max_length):  
+        # for i in range(self.max_length):  
+        for i in range(20):
             # update attention mask
             attn_mask[i] = False
 
