b47e86a4ead03b76f74abcd18426ebfb9b7f2344
svalenta3 <svalenta@gatech.edu>
2023-05-02 23:06:29 -0400

    * Undo changes to run_model.py

--- tasks/run_model.py
+++ tasks/run_model.py
@@ -138,11 +138,10 @@ def main():
     input_data = torch.load('../data/processed/tokenized_input_data.pt')
     target_data = torch.load('../data/processed/tokenized_target_data.pt')
     data = torch.cat((input_data, target_data), dim=1)
-    # train_data, val_data, test_data = random_split(data, [0.8, 0.1, 0.1]) #TODO uncomment
-    train_data, val_data, test_data = random_split(data, [1880, 235, 235]) #TODO delete
+    train_data, val_data, test_data = random_split(data, [0.8, 0.1, 0.1])
 
     # Define hyperparameters
-    EPOCHS = 1
+    EPOCHS = 10
     learning_rate = 1e-3
     input_size = torch.max(input_data).item() + 1
     hidden_size = 128
